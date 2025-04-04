6f3ec473e1caf621a3d0f3ced14901a47e016c83
Aditya7458 <adityarajput4276@gmail.com>
2025-04-04 15:35:51 +0530

    Add Nodemailer for email functionality and implement contact routes

---
+++ controllers/contactController.js
@@ -0,0 +1,54 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+exports.sendContactEmail = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      message,
+      phone,
+      eventDate,
+      selectedPackage,
+      emiPreference,
+    } = req.body;
+    console.log(req.body);
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: process.env.EMAIL_RECIEVER,
+      subject: "New Contact Form Submission",
+      html: `
+                <h3>New Contact Form Message</h3>
+                <p><strong>Name:</strong> ${name}</p>
+                <p><strong>Email:</strong> ${email}</p>
+                <p><strong>Phone:</strong> ${phone}</p>
+                <p><strong>Event Date:</strong> ${
+                  eventDate || "Not specified"
+                }</p>
+                <p><strong>Package:</strong> ${
+                  selectedPackage || "Not selected"
+                }</p>
+                <p><strong>EMI Preference:</strong> ${
+                  emiPreference || "Not selected"
+                }</p>
+                <p><strong>Message:</strong></p>
+                <p>${message}</p>
+            `,
+      };
+      console.log(mailOptions);
+      
+
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Email sending error:", error);
+    res.status(500).json({ error: "Failed to send email" });
+  }
+};
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const connectDB = require('./config/db');
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectDB = require("./config/db");
 const heroRoutes = require("./routes/heroRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 const albumRoutes = require("./routes/albumRoutes");
 const imageRoutes = require("./routes/imageRoutes");
 const authRoutes = require("./routes/authRoutes");
-
+const contactRoutes = require("./routes/contactRoutes");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -20,10 +20,9 @@ app.use("/api/categories", categoryRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/images", imageRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/contact", contactRoutes);
 
 connectDB();
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
-
-
